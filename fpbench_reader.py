743e4593528e4c0c2895b0cfe274a63b7c5071ab
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2017-09-12 17:30:17 -0600

    Demo frontend in fpbench_reader.py

---
+++ fpbench_reader.py
@@ -0,0 +1,12 @@
+from fpbench_parser import parser
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        user_str = input("Enter an expression:\n")
+        print(parser.parse(user_str))
+    else:
+        for e in sys.argv[1:]:
+            with open(e, 'r') as f:
+                print(parser.parse(f))
