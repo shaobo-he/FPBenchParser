19a71ce95680c0ed4ef2cabe6d33afdbf78d2292
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2017-09-12 22:46:06 -0600

    "Fixed" "indentation problem" in example string in lexer

--- fpbench_lexer.py
+++ fpbench_lexer.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
     
     lex.lex()
 
-    lex.input('''(FPCore (t)
-  :name "intro-example"
-  :cite (solovyev-et-al-2015)
-  :pre (<= 0 t 999)
-(/ t (+ t 1)))''')
+    lex.input('(FPCore (t)\n' +
+              '  :name "intro-example"' +
+              '  :cite (solovyev-et-al-2015)' +
+              '  :pre (<= 0 t 999)'+
+              '  (/ t (+ t 1)))')
     for tok in iter(lex.token, None):
         print(repr(tok.type), repr(tok.value))
